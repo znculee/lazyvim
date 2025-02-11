c863a198c563a076f30cc7b18db1ef19b116f339
Xintong Li <znculee@gmail.com>
2025-02-11 18:40:23 +0000

    map Gvdiffsplit!

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,5 +1,10 @@
+vim.keymap.set("n", "<leader>gd", function()
+  vim.fn.feedkeys(vim.api.nvim_replace_termcodes(":Gvdiffsplit! ", true, false, true), "n")
+end, { desc = "Git Diff", silent = true })
+
 return {
   {
     "tpope/vim-fugitive",
+    event = "VeryLazy",
   },
 }
