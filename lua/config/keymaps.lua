9c4673d87951ac4633a89b5c3934eafaf92f94a0
Xintong Li <znculee@gmail.com>
2025-02-14 10:07:53 -0800

    add cmd to trim trailing whitespace

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_create_user_command("TrimTrailspace", function()
+  vim.cmd([[%s/\s\+$//e]])
+end, { desc = "Trim all trailing whitespace" })
