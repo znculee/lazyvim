d77aebc30e73934a6553adb6b181345987c479fd
Xintong Li <znculee@gmail.com>
2025-02-14 10:08:03 -0800

    add mini.hipatterns

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,4 +5,17 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+  {
+    "echasnovski/mini.hipatterns",
+    version = false,
+    event = "VeryLazy",
+    opts = function()
+      local hipatterns = require("mini.hipatterns")
+      return {
+        highlighters = {
+          hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+      }
+    end,
+  },
 }
