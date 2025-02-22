2d960341d6a3a047c8e469a43aa50d4b0bd851b5
Xintong Li <znculee@gmail.com>
2025-02-21 17:30:22 -0800

    set tab

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,32 @@
 vim.g.autoformat = false
 
 local opt = vim.opt
-opt.wrap = true
 opt.clipboard = "unnamedplus"
 opt.relativenumber = false
+
+opt.tabstop = 4
+opt.softtabstop = -1
+opt.shiftwidth = 0
+opt.expandtab = true
+opt.autoindent = true
+opt.smarttab = true
+opt.wrap = true
+opt.breakindent = true
+opt.colorcolumn = "120"
+opt.textwidth = 79
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  command = "set fo-=t fo-=c fo+=q",
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "markdown", "tex", "help", "json" },
+  command = "setlocal cc= lbr",
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "markdown", "gitcommit", "vim", "sh", "html", "css" },
+  command = "setlocal ts=2",
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "asm",
+  command = "setlocal ts=8 ft=nasm",
+})
