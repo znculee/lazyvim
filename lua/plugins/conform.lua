b1bfdf85adabf725f847c8da2f48a36360cd6226
Xintong Li <znculee@gmail.com>
2025-06-20 16:28:40 -0700

    add ruff_fix in conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,7 +4,7 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
       python = function()
-        return vim.fn.executable("ruff") == 1 and { "ruff_format" } or { "isort", "black" }
+        return vim.fn.executable("ruff") == 1 and { "ruff_format", "ruff_fix" } or { "isort", "black" }
       end,
       sh = { "shfmt" },
     },
