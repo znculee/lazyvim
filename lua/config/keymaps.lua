d411051841315c417197423f549eac75460318bb
Xintong Li <znculee@gmail.com>
2026-02-06 10:41:50 -0800

    add a shortcut of copying file path

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,8 @@
 vim.api.nvim_create_user_command("TrimTrailspace", function()
   vim.cmd([[%s/\s\+$//e]])
 end, { desc = "Trim all trailing whitespace" })
+
+vim.keymap.set("n", "<leader>fp", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+  print("Copied: " .. vim.fn.expand("%:p"))
+end, { desc = "Copy absolute file path" })
