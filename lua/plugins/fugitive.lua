5c74c5373f84266a14897bda572e13f4227c24e5
Xintong Li <znculee@gmail.com>
2026-07-24 13:32:51 -0700

    add ge

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -13,6 +13,28 @@ vim.keymap.set("n", "<leader>gdc", function()
   })
 end, { desc = "Git Diff (pick commit)", silent = true })
 
+-- Pick a commit, browse the files it changed, then open the working copy of one
+vim.keymap.set("n", "<leader>ge", function()
+  local fzf = require("fzf-lua")
+  fzf.git_commits({
+    actions = {
+      ["default"] = function(selected)
+        local sha = require("fzf-lua.utils").strip_ansi_coloring(selected[1]):match("^%S+")
+        local root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+        fzf.fzf_exec("git diff-tree --no-commit-id --name-only -r " .. sha, {
+          prompt = "Files@" .. sha .. "> ",
+          preview = "git show --color=always " .. sha .. " -- {}",
+          actions = {
+            ["default"] = function(files)
+              vim.cmd("edit " .. vim.fn.fnameescape(root .. "/" .. files[1]))
+            end,
+          },
+        })
+      end,
+    },
+  })
+end, { desc = "Git Edit (browse commit files)", silent = true })
+
 return {
   {
     "tpope/vim-fugitive",
