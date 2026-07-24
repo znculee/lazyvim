9726a081d81c5a0eea1d97d26be5df3e8e07ac7c
Xintong Li <znculee@gmail.com>
2026-07-24 13:25:47 -0700

    add gdw and gdc

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,17 @@
-vim.keymap.set("n", "<leader>gd", function()
-  vim.fn.feedkeys(vim.api.nvim_replace_termcodes(":Gvdiffsplit! ", true, false, true), "n")
-end, { desc = "Git Diff", silent = true })
+-- Diff current buffer against the working tree / index
+vim.keymap.set("n", "<leader>gdw", "<cmd>Gvdiffsplit!<cr>", { desc = "Git Diff (working tree)", silent = true })
+
+-- Diff current buffer against a commit picked from this file's history
+vim.keymap.set("n", "<leader>gdc", function()
+  require("fzf-lua").git_bcommits({
+    actions = {
+      ["default"] = function(selected)
+        local commit = require("fzf-lua.utils").strip_ansi_coloring(selected[1]):match("^%S+")
+        vim.cmd("Gvdiffsplit! " .. commit)
+      end,
+    },
+  })
+end, { desc = "Git Diff (pick commit)", silent = true })
 
 return {
   {
