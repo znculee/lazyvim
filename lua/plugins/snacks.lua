f9fd1bf7622cb23c1e1c027012d60be6528191bd
Xintong Li <xintongli@apple.com>
2026-08-04 19:40:16 -0700

    open file from lazygit in buffer not tab

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,18 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    lazygit = {
+      -- Open files from lazygit's `e`/`E` edit in the current window (a buffer)
+      -- instead of a new tab. These mirror lazygit's built-in `nvim-remote`
+      -- preset, but use `--remote` in place of `--remote-tab`.
+      config = {
+        os = {
+          edit = '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})',
+          editAtLine = '[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")',
+          openDirInEditor = '[ -z "$NVIM" ] && (nvim -- {{dir}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{dir}})',
+        },
+      },
+    },
     dashboard = {
       preset = {
         header = [[
